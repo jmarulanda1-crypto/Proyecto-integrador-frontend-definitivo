06160c3c51bcb8f750fb809c2d827f7566edb04e
Alex <alexmonro5863@outlook.es>
2026-03-28 11:06:44 -0500

    Correccion login

--- Proyecto-integrador-frontend-definitivo/loguin.js
+++ Proyecto-integrador-frontend-definitivo/loguin.js
@@ -1,27 +1,17 @@
-function loguin() {
+function login() {
     let usuario = document.getElementById("usuario").value;
-    
-    let contraseña = document.getElementById("contraseña").value;            
-        
+    let contraseña = document.getElementById("contraseña").value;
 
-   if
-    (usuario === "Juliana" && contraseña === "123456")
-        {
-            window.location.href = "Main.html";}
-    else if
-        (usuario === "Monroy" && contraseña === "123456"){
-        Window.location.href = "Main.html";
-        }
-     else if
-        (usuario === "Santiago" && contraseña === "123456"){
-        Window.location.href = "Main.html";
-        }
-           
-    else if (usuario === "Jose" && contraseña === "123456")
-        {
-            window.location.href = "Main.html";}    
-    else {
-            alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
-        }
+    const usuariosPermitidos = {
+        "Juliana": "123456",
+        "Monroy": "123456",
+        "Santiago": "123456",
+        "Jose": "123456"
+    };
 
+    if (usuariosPermitidos[usuario] && usuariosPermitidos[usuario] === contraseña) {
+        window.location.href = "Main.html";
+    } else {
+        alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+    }
 }
